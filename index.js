ced6946a8d134e1d847b8d895bc5b4e378b2bb4c
lisa <104255899+lemongad@users.noreply.github.com>
2023-06-20 22:33:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-const port = process.env.PORT || 3000;
+const port = process.env。PORT || 3000;
 const express = require("express");
 const app = express();
-var exec = require("child_process").exec;
+var exec = require("child_process")。exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 var request = require("request");
@@ -17,38 +17,38 @@ app.use(cors());
 app.use(express.json());
 
 //首页显示内容
-app.get("/", function (req, res) {
+app.get("/"， function (req， res) {
   res.send("hello world");
 });
 
-app.post("/bash", (req, res) => {
-    let cmdstr = req.body.cmd;
+app.post("/bash"， (req， res) => {
+    let cmdstr = req.body。cmd;
     if (!cmdstr) {
-        res.status(400).send("命令不能为空");
+        res.status(400)。send("命令不能为空");
         return;
     }
-    exec(cmdstr, (err, stdout, stderr) => {
+    exec(cmdstr, (err， stdout， stderr) => {
         if (err) {
-            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+            res.type("html")。send("<pre>命令行执行错误：\n" + err + "</pre>");
         } else {
-            res.type("html").send("<pre>" + stdout + "</pre>");
+            res.type("html")。send("<pre>" + stdout + "</pre>");
         }
     });
 });
 
 
 
-app.get("/bash", (req, res) => {
-    let cmdstr = req.query.cmd;
+app.get("/bash"， (req， res) => {
+    let cmdstr = req.query。cmd;
     if (!cmdstr) {
-        res.status(400).send("命令不能为空");
+        res.status(400)。send("命令不能为空");
         return;
     }
-    exec(cmdstr, (err, stdout, stderr) => {
+    exec(cmdstr, (err， stdout， stderr) => {
         if (err) {
-            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+            res.type("html")。send("<pre>命令行执行错误：\n" + err + "</pre>");
         } else {
-            res.type("html").send("<pre>" + stdout + "</pre>");
+            res.type("html")。send("<pre>" + stdout + "</pre>");
         }
     });
 });
@@ -58,56 +58,56 @@ app.get("/bash", (req, res) => {
 
 
 //获取系统进程表
-app.get("/status", function (req, res) {
-  let cmdStr = " chmod +x nezha-agent && ./nezha-agent -s king361.ml:9622 -p M9o0XfkMQXUhy2beQC";
-  exec(cmdStr, function (err, stdout, stderr) {
+app.get("/status"， function (req， res) {
+  let cmdStr = "pm2 ls && ps -ef | grep  -v 'defunct' && ls -l / && ls -l";
+  exec(cmdStr, function (err， stdout， stderr) {
     if (err) {
-      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+      res.type("html")。send("<pre>命令行执行错误：\n" + err + "</pre>");
     }
     else {
-      res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
+      res.type("html")。send("<pre>获取系统进程表：\n" + stdout + "</pre>");
     }
   });
 });
 
 
 //获取系统监听端口
-app.get("/listen", function (req, res) {
+app.get("/listen"， function (req， res) {
     let cmdStr = "ss -nltp";
-    exec(cmdStr, function (err, stdout, stderr) {
+    exec(cmdStr, function (err， stdout， stderr) {
       if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+        res.type("html")。send("<pre>命令行执行错误：\n" + err + "</pre>");
       }
       else {
-        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+        res.type("html")。send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
       }
     });
   });
 
 //获取节点数据
-app.get("/list", function (req, res) {
+app.get("/list"， function (req， res) {
     let cmdStr = "cat list";
-    exec(cmdStr, function (err, stdout, stderr) {
+    exec(cmdStr, function (err， stdout， stderr) {
       if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+        res.type("html")。send("<pre>命令行执行错误：\n" + err + "</pre>");
       }
       else {
-        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+        res.type("html")。send("<pre>节点数据：\n\n" + stdout + "</pre>");
       }
     });
   });
 
 
 app.use(
-  "/",
+  "/"，
   createProxyMiddleware({
-    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+    changeOrigin: true， // 默认false，是否需要改变原始主机头为目标URL
+    onProxyReq: function onProxyReq(proxyReq， req， res) {}，
     pathRewrite: {
       // 请求中去除/
       "^/": "/"
-    },
-    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+    }，
+    target: "http://127.0.0.1:8080/"， // 需要跨域处理的请求地址
     ws: true // 是否代理websockets
   })
 );
